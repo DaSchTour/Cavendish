dc1b7c3fc3195d597454ab045f6a8d20444f4aa5
DaSch <dasch@daschmedia.de>
2015-06-17 18:47:02 +0200

    update for MW 1.25

---
+++ Cavendish.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalization file for skin My Skin.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+$messages = array();
+
+/** English
+ * @author DaSch
+ */
+$messages['en'] = array(
+	'cavendish' => "Cavendish",
+	'cavendish-desc' => "Mozilla cavendish theme. Modified by DaSch for MW 1.19 and WeCoWi. Loosely based on the cavendish style by Gabriel Wicke.",
+);
