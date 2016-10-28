6e2beca5c5c16985b3662c4513d7f0e77f131c81
Jack Phoenix <jack@countervandalism.net>
2016-10-28 08:20:40 +0300

    Typofix -- the name of the skin is Cavendish, not Modern
    Also enabled the warning in the PHP entry point so that people know they're supposed to use wfLoadSkin( 'Cavendish' ); to enable the skin instead of require_once $IP . '/skins/Cavendish/Cavendish.php';

--- Cavendish.php
+++ Cavendish.php
@@ -3,12 +3,12 @@
 if ( function_exists( 'wfLoadSkin' ) ) {
 	wfLoadSkin( 'Cavendish' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	$wgMessagesDirs['Modern'] = __DIR__ . '/i18n';
-	/* wfWarn(
-		'Deprecated PHP entry point used for Modern skin. Please use wfLoadSkin instead, ' .
+	$wgMessagesDirs['Cavendish'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for Cavendish skin. Please use wfLoadSkin instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return true;
 } else {
-	die( 'This version of the Modern skin requires MediaWiki 1.25+' );
+	die( 'This version of the Cavendish skin requires MediaWiki 1.25+' );
 }
