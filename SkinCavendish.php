da0e5a84d82d01078ba7be38e71736166e6cb2cb
Jack Phoenix <jack@countervandalism.net>
2017-06-12 18:28:27 +0300

    Massive cleanup
    * cleanup of the main PHP file to reduce the amount of code duplication
    * removed ancient IE6/7 fixes
    * removed RTL fixes -- these should be unnecessary in somewhat modern MediaWikis since CSSJanus handles the flipping etc. automagically; the main stylesheet may need some adjusting, though
    * added /* @embed */ notations to images
    * offloaded Special:Preferences, Special:Version CSS into their own, separate files and made use of $wgResourceModuleSkinStyles to ensure they're loaded by MW core when needed

--- SkinCavendish.php
+++ SkinCavendish.php
@@ -7,19 +7,9 @@ class SkinCavendish extends SkinTemplate {
 	 * @param $out OutputPage object
 	 */
 	function setupSkinUserCss( OutputPage $out ) {
-		global $wgHandheldStyle;
-
 		parent::setupSkinUserCss( $out );
 
 		$out->addModuleStyles( 'skins.cavendish' );
-		if( $wgHandheldStyle ) {
-			// Currently in testing... try 'chick/main.css'
-			$out->addStyle( $wgHandheldStyle, 'handheld' );
-		}
-		$out->addStyle( 'Cavendish/resources/IE60Fixes.css', 'screen', 'IE 6' );
-		$out->addStyle( 'Cavendish/resources/IE70Fixes.css', 'screen', 'IE 7' );
-
-		$out->addStyle( 'Cavendish/resources/rtl.css', 'screen', '', 'rtl' );
 
 		/* README for details */
 		include 'resources/config.php';
@@ -55,23 +45,17 @@ class CavendishTemplate extends MonoBookTemplate {
 ?>
 <div id="internal"></div>
 <!-- Skin version: <?php echo $styleversion ?> //Please leave this for bugtracking purpose//-->
-<div id="globalWrapper" class="<?php echo $action ?>">
+<div id="globalWrapper" class="<?php echo htmlspecialchars( $action, ENT_QUOTES ) ?>">
 	<div id="p-personal" class="portlet">
 		<h5><?php $this->msg( 'personaltools' ) ?></h5>
 		<div class="pBody">
 			<ul<?php $this->html( 'userlangattributes' ) ?>>
-			<?php foreach($this->data['personal_urls'] as $key => $item) {?>
-
-			<li id="<?php echo Sanitizer::escapeId( "pt-$key" ) ?>" class="<?php
-					if ($item['active']) { ?>active <?php } ?>top-nav-element">
-				<span class="top-nav-left">&nbsp;</span>
-				<a class="top-nav-mid <?php echo htmlspecialchars(@$item['class']) ?>"
-				   href="<?php echo htmlspecialchars($item['href']) ?>">
-				   <?php echo htmlspecialchars($item['text']) ?></a>
-				<span class="top-nav-right">&nbsp;</span></li>
-				<?php
-				} ?>
-
+			<?php
+				$personalTools = $this->getPersonalTools();
+				foreach ( $personalTools as $key => $item ) {
+					echo $this->makeListItem( $key, $item );
+				}
+			?>
 			</ul>
 		</div>
 	</div>
@@ -79,82 +63,53 @@ class CavendishTemplate extends MonoBookTemplate {
 		<a name="top" id="contentTop"></a>
 		<h6>
 		<a
-		href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>"
-		title="<?php $this->msg('mainpage') ?>"><?php $this->text('pagetitle') ?></a></h6>
-		<div id="p-cactions" class="portlet"><ul>
-<?php			foreach($this->data['content_actions'] as $key => $tab) {
-					echo '
-				<li id="' . Sanitizer::escapeId( "ca-$key" ) . '"';
-					if( $tab['class'] ) {
-						echo ' class="'.htmlspecialchars($tab['class']).'"';
-					}
-					echo '><a href="'.htmlspecialchars($tab['href']).'"';
-					# We don't want to give the watch tab an accesskey if the
-					# page is being edited, because that conflicts with the
-					# accesskey on the watch checkbox.  We also don't want to
-					# give the edit tab an accesskey, because that's fairly su-
-					# perfluous and conflicts with an accesskey (Ctrl-E) often
-					# used for editing in Safari.
-					if( in_array( $action, array( 'edit', 'submit' ) ) && in_array( $key, array( 'edit', 'watch', 'unwatch' ) ) ) {
-						echo Linker::tooltip( 'ca-'.$key );
-					}
-					else {
-						echo Linker::tooltip( 'ca-'.$key );
-					}
-					echo '>'.htmlspecialchars($tab['text']).'</a></li>';
-				}
-				?>
-			</ul></div>
+		href="<?php echo htmlspecialchars( $this->data['nav_urls']['mainpage']['href'] ) ?>"
+		title="<?php $this->msg( 'mainpage' ) ?>"><?php $this->text( 'pagetitle' ) ?></a></h6>
+		<div id="p-cactions" class="portlet">
+			<ul>
 			<?php
+			foreach ( $this->data['content_actions'] as $key => $tab ) {
+					echo $this->makeListItem( $key, $tab );
+				}
+			?>
+			</ul>
+		</div>
+		<?php
 			// TODO Searchbox Handling
-			$this->searchBox(); ?>
+			$this->searchBox();
+		?>
 	</div>
 	<div id="mBody">
 		<div id="side">
 			<div id="nav">
-<?php //sidebar
-		$sidebar = $this->data['sidebar'];
-		if ( !isset( $sidebar['SEARCH'] ) ) {
-			$sidebar['SEARCH'] = true;
-		}
-		if ( !isset( $sidebar['TOOLBOX'] ) ) {
-			$sidebar['TOOLBOX'] = true;
-		}
-		if ( !isset( $sidebar['LANGUAGES'] ) ) {
-			$sidebar['LANGUAGES'] = true;
-		}
-		foreach ( $sidebar as $boxName => $cont ) {
-			// TODO Searchbox Handling
-			if ( $boxName == 'SEARCH' ) {
-//				$this->searchBox();
-			} elseif ( $boxName == 'TOOLBOX' ) {
-				$this->toolbox();
-			} elseif ( $boxName == 'LANGUAGES' ) {
-				$this->languageBox();
-			} else {
-				$this->customBox( $boxName, $cont );
-			}
-		}
-		?>
-</div>
-</div>
+				<?php $this->renderPortals( $this->data['sidebar'] ); ?>
+			</div>
+		</div>
 		</div><!-- end of SIDE div -->
 		<div id="column-content">
 			<div id="content">
 				<a id="top"></a>
-				<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
-				<h1 id="firstHeading" class="firstHeading"><?php $this->html('title') ?></h1>
+				<?php if ( $this->data['sitenotice'] ) { ?><div id="siteNotice"><?php $this->html( 'sitenotice' ) ?></div><?php } ?>
+				<h1 id="firstHeading" class="firstHeading"><?php $this->html( 'title' ) ?></h1>
 				<div id="bodyContent">
-					<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
-					<div id="contentSub"><?php $this->html('subtitle') ?></div>
-					<?php if($this->data['undelete']) { ?><div id="contentSub2"><?php $this->html('undelete') ?></div><?php } ?>
-					<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
-					<?php if($this->data['showjumplinks']) { ?><div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div><?php } ?>
+					<h3 id="siteSub"><?php $this->msg( 'tagline' ) ?></h3>
+					<div id="contentSub"><?php $this->html( 'subtitle' ) ?></div>
+					<?php if ( $this->data['undelete'] ) { ?><div id="contentSub2"><?php $this->html( 'undelete' ) ?></div><?php } ?>
+					<?php if ( $this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html( 'newtalk' ) ?></div><?php } ?>
+					<div id="jump-to-nav" class="mw-jump"><?php $this->msg( 'jumpto' ) ?> <a href="#column-one"><?php $this->msg( 'jumptonavigation' ) ?></a>, <a href="#searchInput"><?php $this->msg( 'jumptosearch' ) ?></a></div>
 					<!-- start content -->
-					<?php $this->html('bodytext') ?>
-					<?php if($this->data['catlinks']) { $this->html('catlinks'); } ?>
+					<?php
+						$this->html( 'bodytext' );
+						if ( $this->data['catlinks'] ) {
+							$this->html( 'catlinks' );
+						}
+					?>
 					<!-- end content -->
-					<?php if($this->data['dataAfterContent']) { $this->html ('dataAfterContent'); } ?>
+					<?php
+					if ( $this->data['dataAfterContent'] ) {
+						$this->html( 'dataAfterContent' );
+					}
+					?>
 				</div>
 			</div><!-- end of MAINCONTENT div -->
 		</div>
@@ -164,27 +119,27 @@ class CavendishTemplate extends MonoBookTemplate {
 		<table>
 			<tr>
 				<td rowspan="2" class="f-iconsection">
-		<?php //copyright icon
-		if($this->data['copyrightico']) { ?><div id="f-copyrightico"><?php $this->html('copyrightico') ?></div><?php } ?>
+		<?php // copyright icon
+		if ( $this->data['copyrightico'] ) { ?><div id="f-copyrightico"><?php $this->html( 'copyrightico' ) ?></div><?php } ?>
 				</td>
 				<td align="center">
 <?php	// Generate additional footer links
-		$footerlinks = array(
-			'lastmod', 'viewcount', 'numberofwatchingusers', 'credits', 'copyright',
+		$footerLinks = array(
+			'lastmod', 'viewcount', 'credits', 'copyright',
 			'privacy', 'about', 'disclaimer', 'tagline',
 		);
 		$validFooterLinks = array();
-		foreach( $footerlinks as $aLink ) {
-			if( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
+		foreach ( $footerLinks as $aLink ) {
+			if ( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
 				$validFooterLinks[] = $aLink;
 			}
 		}
 		if ( count( $validFooterLinks ) > 0 ) {
 ?>			<ul id="f-list">
 <?php
-			foreach( $validFooterLinks as $aLink ) {
-				if( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
-?>					<li id="f-<?php echo$aLink?>"><?php $this->html($aLink) ?></li>
+			foreach ( $validFooterLinks as $aLink ) {
+				if ( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
+?>					<li id="f-<?php echo $aLink ?>"><?php $this->html( $aLink ) ?></li>
 <?php 			}
 			}
 		}
@@ -201,7 +156,7 @@ class CavendishTemplate extends MonoBookTemplate {
 					?></div>
 					<?php
 					// QR-Code added on option
-					if ($cavendishQRCode) { ?>
+					if ( $cavendishQRCode ) { ?>
 					<div id="qrcode">
 						<a href="http://goqr.me/" style="border:0 none;cursor:default;text-decoration:none;"><img src="http://api.qrserver.com/v1/create-qr-code/?data=<?php echo $QRURL; ?>&#38;size=160x160" height=80 width=80 alt="QR Code generator" title="" /></a>
 					</div>
@@ -220,9 +175,45 @@ class CavendishTemplate extends MonoBookTemplate {
 </div><!-- end of the CONTAINER div -->
 <!-- scripts and debugging information -->
 <?php
-
 		$this->printTrail();
 		echo Html::closeElement( 'body' );
 		echo Html::closeElement( 'html' );
+	}
+
+	/**
+	 * @todo FIXME: this is literal copypasta from MonoBookTemplate.php just
+	 * so that we can comment out one line. That sucks.
+	 * @param array $sidebar
+	 */
+	protected function renderPortals( $sidebar ) {
+		if ( !isset( $sidebar['SEARCH'] ) ) {
+			$sidebar['SEARCH'] = true;
+		}
+		if ( !isset( $sidebar['TOOLBOX'] ) ) {
+			$sidebar['TOOLBOX'] = true;
+		}
+		if ( !isset( $sidebar['LANGUAGES'] ) ) {
+			$sidebar['LANGUAGES'] = true;
+		}
+
+		foreach ( $sidebar as $boxName => $content ) {
+			if ( $content === false ) {
+				continue;
+			}
+
+			// Numeric strings gets an integer when set as key, cast back - T73639
+			$boxName = (string)$boxName;
+
+			if ( $boxName == 'SEARCH' ) {
+				// @todo FIXME: search box handling
+				// $this->searchBox();
+			} elseif ( $boxName == 'TOOLBOX' ) {
+				$this->toolbox();
+			} elseif ( $boxName == 'LANGUAGES' ) {
+				$this->languageBox();
+			} else {
+				$this->customBox( $boxName, $content );
+			}
+		}
 	}
 } // end of class
